db2c4fcfe8faff1ef531ba09d1580ac77f7e595e
geosiso <geovannasiso@usp.br>
2021-06-18 21:07:57 -0300

    Consegui Fazer até tecla AC

--- Index.js
+++ Index.js
@@ -1,46 +1,70 @@
-import React from 'react';
-import { TouchableOpacity as TO, Dimensions, StyleSheet, Text, View, Display } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity as TO, Dimensions, StyleSheet, Text, View, } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons'; 
 
 
 const vw = Dimensions.get("screen").width;
 
-    this.state = {
-        display:"0",
+export function Calculadora() {
+
+    const[resultText,setResultText] = useState("");
+    const[calcText,setCalcText] = useState("");
+
+
+    const onButtonClick = (text) => {
+        
+        console.log(text);
+        if (text == "=") {
+            return calculateResult();
+          }
+        setResultText(resultText + text);
     };
 
-onDigitPress = (digit) => {
-    
-}
+    const calculateResult = () => {};
 
 
-export function Calculadora() {
+
+    const onOperationClick = (text) => {
+
+        if (text == "AC") {
+            setResultText("");
+            setCalcText("0");
+            return;
+        }
+
+        console.log(text);
+        setResultText(resultText + text);
+    }
+
     return (
         <View style={styles.main}>
             <View>
                 <Text style={styles.name}>Calculadora </Text> 
             </View>
             <View style={styles.container} >
-                <Text style={styles.visor} display={this.state.display}/>
+                <Text style={styles.visor}> {calcText}</Text>
+            </View>
+            <View style={styles.container2} >
+                <Text style={styles.visor2}> {resultText} </Text>
             </View>
             <View >
                 <View style={styles.buttons}>
-                <TO onPress={() => {this.onDigitPress("AC")}}>
+                <TO onPress={() => onOperationClick("AC")}>
                     <View style={styles.functions1}>
                         <Text style={styles.letters}>AC</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onOperationClick("+/-")}>
                     <View style={styles.functions1}>
                         <Text style={styles.letters}>+/-</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onOperationClick("%")}>
                     <View style={styles.functions1}>
                         <Text style={styles.letters}>%</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onOperationClick("DEL")}>
                     <View style={styles.functions1}>
                         <Text style={styles.letters}>DEL</Text>
                     </View>
@@ -49,22 +73,22 @@ export function Calculadora() {
                 </View>
                 
                 <View style={styles.buttons}>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(7)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>7</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(8)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>8</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(9)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>9</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onOperationClick("/")}>
                     <View style={styles.functions2}>
                         <Text style={styles.letters}>
                         <FontAwesome5 name="divide" size={24} color="white" />
@@ -75,22 +99,22 @@ export function Calculadora() {
                 
                 </View>
                 <View style={styles.buttons}>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(4)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>4</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(5)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>5</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(6)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>6</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onOperationClick("x")}>
                     <View style={styles.functions2}>
                         <Text style={styles.letters}>x</Text>
                     </View>
@@ -100,22 +124,22 @@ export function Calculadora() {
                 </View>
 
                 <View style={styles.buttons}>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(1)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>1</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(2)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>2</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(3)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>3</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onOperationClick("-")}>
                     <View style={styles.functions2}>
                         <Text style={styles.letters}>-</Text>
                     </View>
@@ -124,22 +148,22 @@ export function Calculadora() {
                 
                 </View>
                 <View style={styles.buttons}>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(0)}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>0</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick(".")}>
                     <View style={styles.numbers}>
                         <Text style={styles.letters2}>.</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onButtonClick("=")}>
                     <View style={styles.functions2}>
                         <Text style={styles.letters}>=</Text>
                     </View>
                 </TO>
-                <TO onPress={() => {}}>
+                <TO onPress={() => onOperationClick("+")}>
                     <View style={styles.functions2}>
                         <Text style={styles.letters}>+</Text>
                     </View>
@@ -176,10 +200,22 @@ const styles=StyleSheet.create({
 
     },
 
+    visor2:{
+        color: "#555555",
+        fontSize:25,
+        textAlign: "right",
+
+    },
+
     container:{
-        paddingTop:65,
+        paddingTop:20,
         paddingRight:25,
-        paddingBottom:10,
+        paddingBottom:5,
+    },
+    container2:{
+        paddingTop:2,
+        paddingRight:25,
+        paddingBottom:11,
     },
 
     buttons:{
